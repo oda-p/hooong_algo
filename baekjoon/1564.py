07e9c018fedd01d9f314122ddb3f2fcd24604214
hooong <tjrwns0529@gmail.com>
2023-11-02 21:56:10 +0900

    1564-2

--- baekjoon/1564.py
+++ baekjoon/1564.py
@@ -1,4 +1,6 @@
-n = int(input())
+import sys
+
+n = int(sys.stdin.readline())
 
 fact = 1
 for i in range(1, n + 1):
@@ -6,6 +8,10 @@ for i in range(1, n + 1):
 
     while fact % 10 == 0:
         fact //= 10
+
+    # rstrip을 사용할 수 있구나!!
+    # -12를 알아내는 것은 노가다인가?
+    # fact = int(str(fact).rstrip("0")[-12:])
 
     fact = int(str(fact)[-100:])
 
