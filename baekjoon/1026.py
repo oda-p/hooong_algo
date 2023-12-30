7b37ba3dc2f79435332d9c3dc39afc5528da1f22
hooong <tjrwns0529@gmail.com>
2023-12-30 13:11:53 +0900

    1026

---
+++ baekjoon/1026.py
@@ -0,0 +1,17 @@
+﻿n = int(input())
+arr_a = list(map(int, input().split(" ")))
+arr_b = list(map(int, input().split(" ")))
+
+arr_a.sort()
+arr_b.sort(reverse=True)
+tmp_1 = 0
+for i in range(n):
+    tmp_1 += arr_a[i] * arr_b[i]
+
+arr_a.sort(reverse=True)
+arr_b.sort()
+tmp_2 = 0
+for i in range(n):
+    tmp_2 += arr_a[i] * arr_b[i]
+
+print(min(tmp_1, tmp_2))
