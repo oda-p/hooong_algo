0fb09c5d3c84d24f1f241e843031e81137b4d07b
hooong <tjrwns0529@gmail.com>
2024-01-07 19:28:25 +0900

    9625

---
+++ baekjoon/9625.py
@@ -0,0 +1,10 @@
+k = int(input())
+
+dp = [[0, 0] for _ in range(k + 1)]
+dp[0] = [1, 0]
+dp[1] = [0, 1]
+
+for i in range(2, k + 1):
+    dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
+
+print(dp[k][0], dp[k][1])
