54366b751299d9fdc7e2b90631fe626f51863fba
hooong <tjrwns0529@gmail.com>
2024-01-30 12:59:50 +0900

    2531 - two pointer

--- baekjoon/2531.py
+++ baekjoon/2531.py
@@ -4,20 +4,26 @@ sushi = []
 for i in range(n):
     sushi.append(int(input()))
 
+s = 0
+e = k - 1
+tmp = sushi[s : e + 1]
+
 max_cnt = 0
 max_target = []
-for i in range(n):
-    tmp = set()
-    cur = i
-    for _ in range(k):
-        tmp.add(sushi[cur])
-        cur = (cur + 1) % n
+while s < n:
+    sushi_cnt = len(set(tmp))
 
-    if len(tmp) > max_cnt:
-        max_cnt = len(tmp)
-        max_target = [tmp]
-    elif len(tmp) == max_cnt:
-        max_target.append(tmp)
+    if sushi_cnt > max_cnt:
+        max_cnt = sushi_cnt
+        max_target = [set(tmp)]
+    elif sushi_cnt == max_cnt:
+        max_target.append(set(tmp))
+
+    s += 1
+    tmp.pop(0)
+
+    e = (e + 1) % n
+    tmp.append(sushi[e])
 
 for target in max_target:
     if c not in target:
